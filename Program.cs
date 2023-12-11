1efe2c1e3e330e81318764dd8e12af2e87ded562
Wankyu Jung <skyship36@gmail.com>
2023-12-11 22:34:30 +0900

    임시저장

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UDPServer server = new UDPServer();
+        UDPClient client = new UDPClient();
+
+        // 서버를 쓰레드로 실행
+        Thread serverThread = new Thread(server.Start);
+        serverThread.Start();
+
+        // 클라이언트를 실행
+        client.Start();
+
+        Console.WriteLine("프로그램 종료");
+    }
+}
